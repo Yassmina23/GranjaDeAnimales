6f5adde2e5ab78e01e23a1ff40cb3f8a53928a19
Yassmina23 <3713321@alu.murciaeduca.es>
2026-03-05 17:20:30 +0100

    Añadido JavaDoc constructor Animal

--- src/animales/Animal.java
+++ src/animales/Animal.java
@@ -16,8 +16,28 @@ public abstract class Animal {
     private char sexo;
     private double peso;
 
-    public Animal(String codigo, String fechaNacimiento, char sexo, double peso) {
+    /**
+     * Constructor de la clase Animal.
+     * Crea un nuevo objeto Animal con un código identificativo, fecha de nacimiento, sexo y peso.
 
+     * Realiza validaciones sobre los parámetros:
+
+     * El código debe tener exactamente 5 caracteres que sean dígitos (0-9) o letras minúsculas (a-z)
+     * El sexo debe ser 'M' (hembra) o 'H' (macho).
+     * El peso debe ser un valor positivo mayor que cero.
+     * La fecha de nacimiento debe estar en formato ISO-8601 (yyyy-MM-dd) válido.
+
+     * Si algún parámetro no cumple estas condiciones, se lanza una excepción IllegalArgumentException
+     * @param codigo el código identificativo del animal, compuesto por 5 caracteres alfanuméricos en minúscula
+     * @param fechaNacimiento la fecha de nacimiento del animal en formato "yyyy-MM-dd"
+     * @param sexo la fecha de nacimiento del animal en formato "yyyy-MM-dd"
+     * @param peso el peso del animal en kilogramos, debe ser mayor que 0
+     * 
+     * @throws IllegalArgumentException si el codigo no cumple el patrón, 
+     * si el sexo es incorrecto, si el peso no es positivo o si la fecha 
+     * no tiene un formato válido
+     */
+    public Animal(String codigo, String fechaNacimiento, char sexo, double peso) {
         LocalDate fecha;
 
         if (!codigo.matches("[0-9a-z]{5}") || (sexo != 'M' && sexo != 'H') || (peso <= 0)) {
